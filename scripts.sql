802cd8932ad72d637b4604110b63c60873ecabaa
Diana Barrero <43156580+dangeRose21@users.noreply.github.com>
2022-09-22 13:57:21 -0400

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,21 +1,3 @@
-CREATE DATABASE recordsPrime;
-USE recordsPrime;
-
-CREATE TABLE artists (
-	id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE albums (
-	id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    year INT,
-    artist_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (artist_id) REFERENCES artists (id)
-);
-
 INSERT INTO artists (name)
 VALUES ('Subtronics');
 
